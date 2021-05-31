eeaef2777277d28833e7e1db7f3d92fb9f6a487b
Sahaj Singh <49692422+SatireSage@users.noreply.github.com>
2021-05-31 11:30:42 -0700

    added visual nodes to canvas
    - Added visual nodes upon clicking on the canvas
    - Automatically draw a line between a previous node and the current node

--- algo.py
+++ algo.py
@@ -1,25 +1,47 @@
 import pygame
 import sys
-# import random
-import math
 import os
-from collections import deque
-from tkinter import messagebox
-from tkinter import Tk
 
-grid = []
-queue, visited = deque(), []
-pathway = []
-
-width, height = 1000, 1000
-window = (width, height)
+os.environ["SDL_VIDEO_CENTERED"] = "1"
 
 pygame.init()
-screen = pygame.display.set_mode(window)
-pygame.display.set_caption("Dijkstra's Visualizer")
-clock = pygame.time.Clock()
 
-# def visualizer():
+X_cords = []
+Y_cords = []
+counter = 0
+White = (225, 225, 225)
+line_width = 1
+node_radius = 5
+screen_width = 800
+screen_height = 800
+screen_size = [screen_width, screen_height]
+screen = pygame.display.set_mode(screen_size)
 
+pygame.display.set_caption("Visualizer")
 
-# visualizer()
+Running = True
+while Running:
+    x, y = pygame.mouse.get_pos()
+
+    for events in pygame.event.get():
+        if events.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif events.type == pygame.MOUSEBUTTONDOWN:
+            X_cords.append(x)
+            Y_cords.append(y)
+            pygame.draw.circle(screen, White, (x, y), node_radius)
+            if len(X_cords) > 1 and len(Y_cords) > 1:
+                start = (X_cords[counter], Y_cords[counter])
+                end = (X_cords[counter+1], Y_cords[counter+1])
+                pygame.draw.line(screen, White, start, end, line_width)
+                counter += 1
+            if len(X_cords) == 5 and len(Y_cords) == 5:
+                start = (X_cords[counter], Y_cords[counter])
+                end = (X_cords[0], Y_cords[0])
+                pygame.draw.line(screen, White, start, end, line_width)
+                pygame.display.update()
+                Running = False
+    pygame.display.update()
+
+click = input("Press")
